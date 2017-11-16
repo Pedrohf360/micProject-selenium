8c3a74592c44d25ff5dbbc7a80547d3d5e2a637e
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2017-11-16 15:47:18 -0200

    Primeiro commit.

---
+++ jsonTest/src/jsonTest/JSONFileWrite.java
@@ -0,0 +1,18 @@
+package jsonTest;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class JSONFileWrite {
+
+	public static void main(String[] args)
+	{
+		JSONObject objJson = new JSONObject();
+		
+		obj.put("Name", "crunchify.com");
+		obj.put("Author", "App Shah");
+	}
+}
